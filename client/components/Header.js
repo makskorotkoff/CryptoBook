f777818fb1bad7f5ee3d17b7664740692294893f
makskorotkoff <maks.korotkoff@gmail.com>
2023-07-03 17:28:18 +0300

    added login button

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,7 +1,24 @@
-import { Menu } from "semantic-ui-react";
+import { Button, Menu } from "semantic-ui-react";
 import Link from "next/link";
+import { useState } from "react";
 
 const Header = () => {
+  const [currentAccount, setCurrentAccount] = useState();
+
+  const handleLoginClick = async () => {
+    const { ethereum } = window;
+    if (!ethereum) {
+      alert("У вас нет метамаска");
+    }
+    try {
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <Menu style={{ marginTop: "20px" }}>
       <Link href="/">
@@ -13,6 +30,19 @@ const Header = () => {
       <Link href="/show">
         <Menu.Item>Посмотреть контракт</Menu.Item>
       </Link>
+      <Menu.Item position="right">
+        {!currentAccount ? (
+          <Button primary onClick={handleLoginClick}>
+            Вход
+          </Button>
+        ) : (
+          <Link href="/user">
+            <Button primary onClick={handleLoginClick}>
+              {currentAccount}
+            </Button>
+          </Link>
+        )}
+      </Menu.Item>
     </Menu>
   );
 };
